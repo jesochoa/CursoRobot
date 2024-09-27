1b1b4317625da2c3ffa799762aac719576bc70e8
jesochoa <jsochoa8@gmail.com>
2024-09-27 10:01:23 +0200

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+
+constexpr gpio_num_t LED_PIN = GPIO_NUM_2;
 
 extern "C" void app_main();
 
-
 void app_main() 
 {
-// hola 2 prueba de github
+    // Setup
+    gpio_config_t config;
+    config.mode = GPIO_MODE_OUTPUT;
+    config.pin_bit_mask = (1<<LED_PIN);
+    config.intr_type = GPIO_INTR_DISABLE;
+    config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    config.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&config);
+
+    //
+    // Init GPIO El que querais as INPUT
+
+    
+    // Loop
+    while(1)
+    {   
+
+        // Set LED_PIN to INPUT value.
+        // If INPUT == 1 then LED_PIN = 1
+        // If INPUT == 0 then LED_PIN = 0
+
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(300));
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(300));
+    }
 
 }
