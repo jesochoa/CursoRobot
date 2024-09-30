bb4e196e33cb388aa57b1f09a4ce0cda17cb6059
jesochoa <jsochoa8@gmail.com>
2024-09-30 20:58:07 +0200

    Con LEDC no funciona

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <freertos/task.h>
 #include <driver/gpio.h>
 
-//usamos el led interno que esta en el GPIO_2
-constexpr gpio_num_t LED_PIN = GPIO_NUM_2;
-constexpr gpio_num_t BUTTOM_PIN = GPIO_NUM_13; //Boton
+// Tema 7 controlador pwm con ledc
+#include <driver/ledc.h>
 
-//Como usamos C++ con esta función conseguimos que el void app_main() no cambie de nombre
+constexpr gpio_num_t LED_PIN = GPIO_NUM_2; //Donde conectamos el LED
+constexpr gpio_num_t BUTTON_PIN = GPIO_NUM_13; //Donde conectamos el Boton
+
+//Como usamos C++ con esta declaración conseguimos que el void app_main() no cambie de nombre
 extern "C" void app_main();
 
 void app_main() 
@@ -24,12 +26,35 @@ void app_main()
     //
     // Init GPIO_13 como INPUT
     config.mode = GPIO_MODE_INPUT; //LO DECLARO COMO ENTRADA
-    config.pin_bit_mask = (1<<BUTTOM_PIN); //Corro el 1 13 posiciones
+    config.pin_bit_mask = (1<<BUTTON_PIN); //Corro el 1 13 posiciones
+    /*
     config.intr_type = GPIO_INTR_DISABLE; // no hace falta ponerla ya que esta igual al LED
     config.pull_down_en = GPIO_PULLDOWN_DISABLE; // no hace falta ponerla ya que esta igual al LED
+    */
     config.pull_up_en = GPIO_PULLUP_ENABLE; // Activo la resistencia PULLUP
     gpio_config(&config); // Grabo la configuración del pulsador
     
+    //Tema 5 configuro el TIMER lo llamo ledcTimer
+    ledc_timer_config_t ledcTimer;
+    ledcTimer.speed_mode = LEDC_LOW_SPEED_MODE;
+    ledcTimer.freq_hz =  1000; //Pruebo con 500Hz
+    ledcTimer.duty_resolution = LEDC_TIMER_13_BIT; //Pongo una resolución de 13Bits 
+    ledcTimer.clk_cfg = LEDC_AUTO_CLK ; // Pongo automatico
+    ledcTimer.timer_num = LEDC_TIMER_0; //Utilizo el primer Timer
+    ledc_timer_config(&ledcTimer); //Mando la configuración
+
+    ledc_channel_config_t ledcChannel;
+    ledcChannel.channel = LEDC_CHANNEL_0; //Elijo el canal 0
+    ledcChannel.gpio_num = GPIO_NUM_12;
+    ledcChannel.duty = 4096; //Como es 
+    ledcChannel.timer_sel = LEDC_TIMER_0;
+    ledcChannel.intr_type = LEDC_INTR_DISABLE;
+    ledcChannel.speed_mode = LEDC_LOW_SPEED_MODE;
+    ledcChannel.flags.output_invert = 0; //Pongo 0 para que no me invierta la señal
+    ledc_channel_config(&ledcChannel);
+
+
+
     // Loop
     while(1)
     {   
@@ -43,8 +68,8 @@ void app_main()
         // gpio_set_level(LED_PIN, 0);
         // vTaskDelay(pdMS_TO_TICKS(1000));
 
-        gpio_set_level(LED_PIN , !gpio_get_level(BUTTOM_PIN)); //si se pulsa el led se enciende
-        vTaskDelay(pdMS_TO_TICKS(100)); // espera para que no de error Task watchdog
+        gpio_set_level(LED_PIN , !gpio_get_level(BUTTON_PIN)); //si se pulsa el led se enciende
+        vTaskDelay(pdMS_TO_TICKS(10)); // espera para que no de error Task watchdog
     }
 
 }
