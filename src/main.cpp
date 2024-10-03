02e234ddaa0924881817bb6b09d788b6fc297214
jesochoa <jsochoa8@gmail.com>
2024-10-03 17:14:36 +0200

    Arreglado los rebotes del pulsador

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include <freertos/FreeRTOS.h> //Siempre se pone la primera si no da error
 #include <freertos/task.h>
 #include <driver/gpio.h>
+#include <stdio.h>
 
 // Tema 7 controlador pwm con ledc
 #include <driver/ledc.h>
 
-constexpr gpio_num_t LED_PIN = GPIO_NUM_2; //Donde conectamos el LED
+// constexpr gpio_num_t LED_PIN = GPIO_NUM_2; //Donde conectamos el LED
 constexpr gpio_num_t BUTTON_PIN = GPIO_NUM_13; //Donde conectamos el Boton
 
 //Como usamos C++ con esta declaración conseguimos que el void app_main() no cambie de nombre
@@ -17,21 +18,21 @@ void app_main()
     // Setup
     
     gpio_config_t config;
+    /*
     config.mode = GPIO_MODE_OUTPUT; // Lo declaro como salida
     config.pin_bit_mask = (1<<LED_PIN); // Desplazo 2 posiciones el 1
     config.intr_type = GPIO_INTR_DISABLE; // Desabilito la interrupción
     config.pull_down_en = GPIO_PULLDOWN_DISABLE;
     config.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&config); //Grabo la configuracion
-    
+    */
     
     // Init GPIO_13 como INPUT
-    
     config.mode = GPIO_MODE_INPUT; //LO DECLARO COMO ENTRADA
-    config.pin_bit_mask = (1<<BUTTON_PIN); //Corro el 1 13 posiciones
+    config.pin_bit_mask = (1<<BUTTON_PIN); //Corro el 1 13 posiciones par elejir GPIO13
     
-    //config.intr_type = GPIO_INTR_DISABLE; // no hace falta ponerla ya que esta igual al LED
-    //config.pull_down_en = GPIO_PULLDOWN_DISABLE; // no hace falta ponerla ya que esta igual al LED
+    config.intr_type = GPIO_INTR_DISABLE; // no hace falta ponerla ya que esta igual al LED
+    config.pull_down_en = GPIO_PULLDOWN_DISABLE; // no hace falta ponerla ya que esta igual al LED
     
     config.pull_up_en = GPIO_PULLUP_ENABLE; // Activo la resistencia PULLUP
     gpio_config(&config); // Grabo la configuración del pulsador
@@ -49,7 +50,7 @@ void app_main()
     ledc_channel_config_t ledcChannel = {};
     ledcChannel.channel = LEDC_CHANNEL_0; //Elijo el canal 0
     ledcChannel.gpio_num = GPIO_NUM_22;
-    ledcChannel.duty = 4096; //Lo dejo 50%
+    ledcChannel.duty = 0; //Lo dejo 50% 4096 lo cambio 0%
     ledcChannel.timer_sel = LEDC_TIMER_0;
     ledcChannel.intr_type = LEDC_INTR_DISABLE;
     ledcChannel.speed_mode = LEDC_LOW_SPEED_MODE;
@@ -57,6 +58,7 @@ void app_main()
     ledc_channel_config(&ledcChannel);
 
     uint32_t cnt = 0;
+    uint32_t cnt_1 = 0 ;
 
     // Loop
     while(1)
@@ -71,17 +73,66 @@ void app_main()
         // gpio_set_level(LED_PIN, 0);
         // vTaskDelay(pdMS_TO_TICKS(1000));
 
-        gpio_set_level(LED_PIN , !gpio_get_level(BUTTON_PIN)); //si se pulsa el led se enciende
+        // gpio_set_level(LED_PIN , !gpio_get_level(BUTTON_PIN)); //si se pulsa el led se enciende
+        
+        
+
+        if (!gpio_get_level(BUTTON_PIN))
+        {
+            while (!gpio_get_level(BUTTON_PIN)){vTaskDelay(pdMS_TO_TICKS(10));} //para arreglar el rebote de pulsador
+            
+            cnt++;
+                       
+            if (cnt > 4)
+            {
+            cnt = 0; // Pone a 0 el contador
+            }
+        }
+
+        if (cnt == 0)
+        {
+            ledc_set_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 0);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0); 
+            printf("Contador ");
+        }
+
+        if (cnt == 1)
+        {
+            ledc_set_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 8192);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0); 
+        }
+
+        if (cnt == 2)
+        {
+            ledc_set_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 4096);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0); 
+        }
+        
+        if (cnt == 3)
+        {
+            ledc_set_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 1000);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0); 
+        }
+
+        if (cnt == 4)
+        {
+            while (cnt_1 < 8192)
+            {
+                ledc_set_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, cnt_1++);
+                ledc_update_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0);
+                //vTaskDelay(pdMS_TO_TICKS(10)); 
+            }
+            cnt_1 = 0;
+            cnt = 0 ;
+        }
+            
+            
+
+        
         
         vTaskDelay(pdMS_TO_TICKS(10)); // espera para que no de error Task watchdog
+        
 
-        ledc_set_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, cnt++);
-        ledc_update_duty(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0);
-
-        if (cnt > 8192)
-        {
-            cnt = 0;
-        }
     }
 
 }
